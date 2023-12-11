58816cdb56f18d9bd54b71ddc041907d060c54a8
Faaris <faaris.iqbal@gmail.com>
2023-12-11 14:10:18 -0800

    making bullet progress

--- main.py
+++ main.py
@@ -34,6 +34,18 @@ game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'images')
 snd_folder = os.path.join(game_folder, 'sounds')
 
+class Cooldown():
+    def __init__(self):
+        self.current_time = 0
+        self.event_time = 0
+        self.delta = 0
+    def ticking(self):
+        self.current_time = (pg.time.get_ticks())/1000
+        self.delta = self.current_time - self.event_time
+    def timer(self):
+        self.current_time = (pg.time.get_ticks())/1000
+cd = Cooldown()
+
 # Game class
 class Game:
     def __init__(self):
@@ -147,6 +159,17 @@ class Game:
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect(topleft=(x, y))
         self.screen.blit(text_surface, text_rect)
+
+
+    def shoot(self):
+            # if the player presses space and the timer has been running for more than 0.3 seconds, create a bullet and shoot it
+            if pg.key.get_pressed()[pg.K_SPACE] and self.cd.delta > 0.3:
+                self.cd.event_time = pg.time.get_ticks()/1000
+                b = Bullet()
+                self.all_sprites.add(b)
+                self.all_bullets.add(b)
+                # sound effect
+                pg.mixer.Channel(1).play(pg.mixer.Sound(os.path.join(snd_folder, 'pew.wav')))
 
     def events(self):
         # Handle game events
